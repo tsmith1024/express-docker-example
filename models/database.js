7a0e554bad2244000856b146097a2840b2346cb8
Taylor <tsmith1024@gmail.com>
2022-10-06 09:17:30 -0500

    add comment explaining host name

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const db = new Sequelize("dockerExpressDemo", "postgres", "postgres", {
-  host: "db",
+  host: "db", // host is 'db' because of name of linked service in docker-compose
   dialect: "postgres",
 });
 
